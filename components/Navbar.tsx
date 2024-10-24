6d8b02492dd14ff86ec30f329e60f0455cb0c039
A8fH4K7jL9pX3mT2bY5qN6vZ1uG0rW2Q8nL3B7 <hackerinside69@outlook.com>
2024-10-24 20:16:38 +0700

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
-import NezokaLogo from '@/assets/images/nezoka.webp';
 import {
   Sheet,
   SheetContent,
